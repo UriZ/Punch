313432a18dafdeaf4758aab4187d5782736205a1
Yuval Adam <yuv.adm@gmail.com>
2012-05-29 18:36:15 +0300

    update statuc file serving

--- punch/punch/settings.py
+++ punch/punch/settings.py
@@ -60,11 +60,11 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = os.path.join(SITE_ROOT, 'static')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
-STATIC_URL = 'punchapp/static/'
+STATIC_URL = '/static/'
 
 # URL prefix for admin static files -- CSS, JavaScript and images.
 # Make sure to use a trailing slash.
--- punch/punch/urls.py
+++ punch/punch/urls.py
@@ -1,4 +1,4 @@
-
+from django.conf import settings
 from django.conf.urls.defaults import patterns, include, url
 
 # Uncomment the next two lines to enable the admin:
@@ -14,7 +14,7 @@ urlpatterns = patterns('',
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
     url(r'^users/$', 'punchapp.views.users'),
     url(r'^users/(?P<user_id>\d+)/$', 'punchapp.views.CollectionsByUser'),
     url(r'^collections/$', 'punchapp.views.index'),
@@ -25,6 +25,6 @@ url(r'^admin/', include(admin.site.urls)),
     url(r'^boo/(?P<punch_id>\d+)/$','punchapp.views.make_boo'),
     
     url(r'^follow/(?P<user_id>\d+)/(?P<follow_id>\d+)/$','punchapp.views.follow'),
-
+    (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
 )
 
